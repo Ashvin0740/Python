27c75b53ca688a4c0b22a4efa7ba21cc07476581
Ashvin0740 <67723298+Ashvin0740@users.noreply.github.com>
2020-07-30 20:09:54 +0530

    function
    how to use function in python.

---
+++ function.py
@@ -0,0 +1,7 @@
+def function1(a,b):
+    """this is my frist function program"""
+    average = (a+b)/2
+  
+    print("hello you are in funtion 1",average)
+function1(5,6)
+print(function1.__doc__)
