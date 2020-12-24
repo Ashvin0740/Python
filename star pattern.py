091ca5c9d6b98a66c1a7dbd98b55640c2fb69fab
Ashvin0740 <67723298+Ashvin0740@users.noreply.github.com>
2020-12-24 08:47:17 +0530

    star pattern program
    star pattern program

--- star pattern.py
+++ star pattern.py
@@ -1,7 +1,7 @@
 # stra pattern program
 num = int(input("how many rows you want :"))
 print("inter 0 or 1 ")
-bool_var = input("1 for true value and 0 for false value")
+bool_var = input("1 for true value and 0 for false value : ")
 if bool_var == "1":
     for i in range(0,num+1):
           print("*" *i)
